1b3f4eee36adaddc1f957699597d5f1a47561794
Jibs <iOpski@users.noreply.github.com>
2025-06-02 11:18:37 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.recordapp',
+  appName: 'RecordApp',
+  webDir: 'dist/public'
+};
+
+export default config;
